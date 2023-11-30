d8fbbb428b6dcfda47bbce61177fd7ef77b75dd8
godhyzzang <amksh0914@gmail.com>
2023-11-30 15:31:23 +0900

    실습 2 : 구조체 연산

--- ex1130/ex1130/main.c
+++ ex1130/ex1130/main.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> //strcpy위해.
+#include <math.h>
 
-struct student {
-	int id; //학번
-	char name[50]; //이름
-	float score; //학점
-}
-
+struct point {
+	int x;
+	int y;
+};
 int main(void) {
-	//Instance 선언
-	struct student s1 = {1910655, "양하연", 4.3};
+	struct point p1, p2;
+	int xdiff, ydiff;
+	double dist;
 
-	printf("학번: %i, 이름 :%s, 학점 : %i" :, s1.id, s1.name, s1.score);
-	//다른값 변경
-	s1.id = 8910655; 
-	//s1.name = "김수현";
-	strcpy(s1.name,"bongjun");
-	s1.score = 4.0;
-	//출력 
-	printf("학번: %i, 이름 :%s, 학점 : %i" :, s1.id, s1.name, s1.score);
+	printf("input p1 coordinate(x y) :" );
+	scanf("% d % d" ,& p1.x, &p1.y);
+	printf(“input p2 coordinate(x y) : “);
+	scanf(“ % d % d” & p2.x, &p2.y);
+
+	//distance
+	
+	int xdiff = (p2.x - p1.x);
+	int ydiff = (p2.y - p1.y);
+	dist = sqrt((xdiff * xdiff) + (ydiff * ydiff));
+
+		printf("distance : %f\n", dist);
+
+		return 0;
+
 }
